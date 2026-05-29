f13091b28a0d9bc92d38908abdb543c9458d6e97
Saba Miró <sabam1015@gmail.com>
2026-05-29 16:40:32 -0300

    config: set estreno date to 2026-06-05

--- config/estreno.ts
+++ config/estreno.ts
@@ -1,2 +1,2 @@
-export const ESTRENO_DATE = new Date("2026-06-15T21:00:00-03:00");
+export const ESTRENO_DATE = new Date("2026-06-05T21:00:00-03:00");
 export const YOUTUBE_ID = ""; // ej: "dQw4w9WgXcQ"
